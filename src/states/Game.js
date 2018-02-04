74f553f78f8f144b3dc247298664efaa83429425
FEDQK <vovand1996@gmail.com>
2018-02-04 16:39:37 +0200

    add(Game): touch move player

--- src/states/Game.js
+++ src/states/Game.js
@@ -16,29 +16,46 @@ export default class extends Phaser.State {
     this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space')
     this.background.autoScroll(0, 30)
 
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
-      font: '40px Bangers',
-      fill: '#77BFA3',
-      smoothed: false
-    })
+    this.player = this.add.sprite(this.game.world.centerX, this.game.world.height - 50, 'player')
+    this.player.scale.setTo(0.6)
+    this.player.anchor.setTo(0.5)
+    this.game.physics.arcade.enable(this.player)
+    this.player.body.collideWorldBounds = true
 
-    banner.padding.set(10, 16)
-    banner.anchor.setTo(0.5)
 
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+    // const bannerText = 'Phaser + ES6 + Webpack'
+    // let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
+    //   font: '40px Bangers',
+    //   fill: '#77BFA3',
+    //   smoothed: false
+    // })
+    //
+    // banner.padding.set(10, 16)
+    // banner.anchor.setTo(0.5)
+    //
+    // this.mushroom = new Mushroom({
+    //   game: this.game,
+    //   x: this.world.centerX,
+    //   y: this.world.centerY,
+    //   asset: 'mushroom'
+    // })
+    //
+    // this.game.add.existing(this.mushroom)
+  }
 
-    this.game.add.existing(this.mushroom)
+  update () {
+    this.player.body.velocity.x = 0
+
+    if(this.game.input.activePointer.isDown) {
+      let targetX = this.game.input.activePointer.position.x
+      let direction = targetX >= this.game.world.centerX ? 1 : -1
+      this.player.body.velocity.x = direction * this.PLAYER_SPEED
+    }
   }
 
   render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
-    }
+    // if (__DEV__) {
+    //   this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    // }
   }
 }
