386e8cdb1f24906f6de07ec42cc60ef7dafce97b
FEDQK <vovand1996@gmail.com>
2018-02-05 21:45:50 +0200

    add(Enemy): create emitter for enemy particle

--- src/prefabs/Enemy.js
+++ src/prefabs/Enemy.js
@@ -29,5 +29,14 @@ export default class extends Phaser.Sprite {
   damage (amount) {
     super.damage(amount)
     this.play('getHit')
+
+    if (this.health <= 0) {
+      let emitter = this.game.add.emitter(this.x, this.y, 100)
+      emitter.makeParticles('enemyParticle')
+      emitter.minParticleSpeed.setTo(-200, -200)
+      emitter.maxParticleSpeed.setTo(200, 200)
+      emitter.gravity = 0
+      emitter.start(true, 500, null, 100)
+    }
   }
 }
