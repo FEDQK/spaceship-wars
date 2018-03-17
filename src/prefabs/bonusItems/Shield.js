3dfb4bead9d900e8299b148b9df77121bf5c4fa6
FEDQK <vovand1996@gmail.com>
2018-03-17 22:49:07 +0200

    update(Shield): overlap and sound shield down

--- src/prefabs/bonusItems/Shield.js
+++ src/prefabs/bonusItems/Shield.js
@@ -1,9 +1,10 @@
 import Phaser from 'phaser'
 
-export default class extends Phaser.Sprite {
+export default class Shield extends Phaser.Sprite {
   constructor (game, x, y, asset) {
     super(game, x, y, asset)
     this.game = game
+    this.game.physics.arcade.enable(this)
     this.animations.add('createShield', [0, 1, 2], 8, false)
     this.scale.setTo(0.6)
     this.anchor.setTo(0.5)
@@ -17,5 +18,10 @@ export default class extends Phaser.Sprite {
     const volume = 0.1
     this.shieldUp = this.game.sound.add('shieldUp', volume)
     this.shieldDown = this.game.sound.add('shieldDown', volume)
+  }
+
+  damage (amount) {
+    super.damage(amount)
+    this.shieldDown.play()
   }
 }
